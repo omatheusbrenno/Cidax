ffe2890eb2739058dfd711d67656917dc7ccb492
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-03-30 14:41:24 -0300

    feat: Configurando enums para entidade Category

---
+++ src/Backend/Cidax.Domain/Enums/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Cidax.Domain.Enums
+{
+    public enum Category
+    {
+        Farmacia = 1,
+        Hospital = 2,
+        Escola = 3,
+        Restaurante = 4,
+        Mercado = 5,
+        Moradia = 6,
+        Outros = 7,
+    }
+}
