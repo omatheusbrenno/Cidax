f900d8842a7098ad9407d6946eae143f88df1459
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-03-30 10:04:20 -0300

    refactor: alterando modelo de requisição "Coordinates" para "Latitude" e "Longitude"

---
+++ src/Shared/Cidax.Communication/Requests/RequestRegisterLocationJson.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Cidax.Communication.Requests
+{
+    public class RequestRegisterLocationJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
