0b5148c26fd07af747f500634bb15fcc7dc4c29a
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-03-31 10:15:28 -0300

    refactor: Alterando a ordem de Latitude-Longitude para Longitude-Latitude

--- src/Shared/Cidax.Communication/Requests/RequestRegisterLocationJson.cs
+++ src/Shared/Cidax.Communication/Requests/RequestRegisterLocationJson.cs
@@ -6,7 +6,7 @@ namespace Cidax.Communication.Requests
     {
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
-        public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
