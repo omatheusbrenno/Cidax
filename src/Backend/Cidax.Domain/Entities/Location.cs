55dddbfd6a6546e31ef73040c48695e0632c7055
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-03-31 09:24:32 -0300

    feat: Criando entidade Location

---
+++ src/Backend/Cidax.Domain/Entities/Location.cs
@@ -0,0 +1,13 @@
+﻿using Cidax.Domain.Enums;
+using System.Drawing;
+
+namespace Cidax.Domain.Entities
+{
+    public class Location
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Category? Category { get; set; }
+        public Point Point { get; set; }
+    }
+}
