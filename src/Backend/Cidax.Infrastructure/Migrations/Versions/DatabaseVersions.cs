b3dc0658466650805e8272b50f7a3356a7267343
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-04-02 11:40:59 -0300

    feat: Configurando migration para criação de tabela Locations no Banco de Dados

---
+++ src/Backend/Cidax.Infrastructure/Migrations/Versions/DatabaseVersions.cs
@@ -0,0 +1,7 @@
+﻿namespace Cidax.Infrastructure.Migrations.Versions
+{
+    public abstract class DatabaseVersions
+    {
+        public const int TABLE_LOCATION = 1;
+    }
+}
---
+++ src/Backend/Cidax.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace Cidax.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_LOCATION, "Cria tabela para salvar informações de Localizações")]
+    public class Version0000001 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Locations")
+                .WithColumn("Id").AsGuid().PrimaryKey()
+                .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("Category").AsInt32().Nullable()
+                .WithColumn("Point").AsCustom("GEOGRAPHY(Point, 4326)").NotNullable();
+        }
+    }
+}
